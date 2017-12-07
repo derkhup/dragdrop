c2643a8dde73270bce0cdfb04d78f08b3d02c574
Your Name <you@example.com>
2017-12-07 14:04:39 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<style>
+#div1 {
+    width: 350px;
+    height: 70px;
+    padding: 10px;
+    border: 1px solid #aaaaaa;
+}
+</style>
+
+</head>
+<body>
+
+<p>Drag the mouse into the rectangle:</p>
+
+<div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+<br>
+
+
+</body>
+</html>
