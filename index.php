ae4858ccaacf2cf98d84d184ed88432f54f7fb4f
Your Name <you@example.com>
2017-12-07 14:14:19 +0100

    mouse in the box

--- index.php
+++ index.php
@@ -4,20 +4,34 @@
 <style>
 #div1 {
     width: 350px;
-    height: 70px;
+    height: 350px;
     padding: 10px;
-    border: 1px solid #aaaaaa;
+    border: 2px solid #aaaaaa;
 }
 </style>
+<script>
+function allowDrop(ev) {
+    ev.preventDefault();
+}
 
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+</script>
 </head>
 <body>
 
-<p>Drag the mouse into the rectangle:</p>
+<p>Drag the Mouse into the rectangle:</p>
 
 <div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
 <br>
-
+<img id="drag1" src="images.jpg" draggable="true" ondragstart="drag(event)">
 
 </body>
 </html>
